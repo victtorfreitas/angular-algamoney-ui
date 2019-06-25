a6324820892805855fc6890b197032021cd217ff
Victtor <victtorfreitas95@gmail.com>
2019-06-24 22:14:04 -0300

    Iniciando projeto já configurado primeng

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  title = 'algamoney-ui';
+  items: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {label: 'Stats', icon: 'fa fa-fw fa-bar-chart'},
+      {label: 'Calendar', icon: 'fa fa-fw fa-calendar'},
+      {label: 'Documentation', icon: 'fa fa-fw fa-book'},
+      {label: 'Support', icon: 'fa fa-fw fa-support'},
+      {label: 'Social', icon: 'fa fa-fw fa-twitter'}
+    ];
+  }
+}
