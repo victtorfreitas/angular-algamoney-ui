05a04cf99b286594aa349bdf89b221fbde9c4126
Victtor <victtorfreitas95@gmail.com>
2020-09-20 18:51:50 -0300

    Adiciona componente de cadastro de lançamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,27 @@ import {AppComponent} from './app.component';
 import {InputTextModule} from 'primeng/inputtext';
 import {TableModule} from 'primeng/components/table/table';
 import {ButtonModule} from 'primeng/button';
-import {TooltipModule} from 'primeng/primeng';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasComponent } from './pessoas/pessoas.component';
+import {InputTextareaModule, TooltipModule} from 'primeng/primeng';
+import {LancamentosPesquisaComponent} from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {PessoasComponent} from './pessoas/pessoas.component';
+import {LancamentosCadastroComponent} from './lancamentos-cadastro/lancamentos-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
-    PessoasComponent
+    PessoasComponent,
+    LancamentosCadastroComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
     ButtonModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
