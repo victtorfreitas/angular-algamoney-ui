04b9fddc9c6f34d4ffc65ce4c499f8302f060bcf
Victtor <victtorfreitas95@gmail.com>
2020-09-20 10:32:04 -0300

    Adiciona navbar componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import {TableModule} from 'primeng/components/table/table';
 import {ButtonModule} from 'primeng/button';
 import {TooltipModule} from 'primeng/primeng';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent
+    LancamentosPesquisaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
