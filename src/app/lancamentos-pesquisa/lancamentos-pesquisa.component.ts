bc4f434b49be2de083fd8f3c2b368749c6beb502
Victtor <victtorfreitas95@gmail.com>
2020-09-20 16:55:22 -0300

    Adiciona pipe data e moeda

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -13,23 +13,23 @@ export class LancamentosPesquisaComponent implements OnInit {
   ngOnInit() {
     this.lancamentos = [
       {
-        tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-        dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'
+        tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2017, 6, 30),
+        dataPagamento:  new Date(2017, 7, 1), valor: 4.55, pessoa: 'Padaria do José'
       }, {
-        tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/06/2017',
-        dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'
+        tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento:  new Date(2017, 6, 10),
+        dataPagamento:  new Date(2017, 6, 9), valor: 80000, pessoa: 'Atacado Brasil'
       }, {
-        tipo: 'DESPESA', descricao: 'Compra de livro', dataVencimento: '31/06/2017',
-        dataPagamento: '19/06/2017', valor: 14.55, pessoa: 'Livraria do João'
+        tipo: 'DESPESA', descricao: 'Compra de livro', dataVencimento:  new Date(2017, 6, 31),
+        dataPagamento: new Date(2017, 6, 19), valor: 14.55, pessoa: 'Livraria do João'
       }, {
-        tipo: 'RECEITA', descricao: 'Venda de cadernos', dataVencimento: '10/07/2017',
-        dataPagamento: '09/07/2017', valor: 50.45, pessoa: 'Livraria Brasil'
+        tipo: 'RECEITA', descricao: 'Venda de cadernos', dataVencimento:  new Date(2017, 7, 10),
+        dataPagamento:  new Date(2017, 7, 9), valor: 50.45, pessoa: 'Livraria Brasil'
       }, {
-        tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: '05/08/2017',
+        tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento:  new Date(2017, 8, 5),
         dataPagamento: '04/08/207', valor: 45000.00, pessoa: 'Honda'
       }, {
-        tipo: 'DESPESA', descricao: 'Compra de carro', dataVencimento: '08/08/2017',
-        dataPagamento: '08/08/2017', valor: 55000.00, pessoa: 'Ford'
+        tipo: 'DESPESA', descricao: 'Compra de carro', dataVencimento:  new Date(2017, 8, 8),
+        dataPagamento:  new Date(2017, 8, 8), valor: 55000.00, pessoa: 'Ford'
       },
     ];
   }
